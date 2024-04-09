c746c079f3eb7b424e106ee61e8f71294b08f8e6
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-04-09 15:29:42 +0530

    final commit

--- server.js
+++ server.js
@@ -2,33 +2,19 @@ const express = require("express");
 const articleRouter = require("./routes/articles");
 const mongoose = require("mongoose");
 const Article = require("./models/article");
+const methodOverride = require('method-override')
 const app = express();
 
 mongoose.connect("mongodb://localhost/MohitscornerDataBase");
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Testing Description",
-    },
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Testing Description",
-    },
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Testing Description",
-    },]
+app.use(methodOverride('_method'))
+app.get("/", async (req, res) => {
+  const article = await Article.find().sort({ createdAt: 'desc' });
 
-  res.render("articles/index", { articles: articles }
+  res.render("articles/index", { articles: article }
   )
-  app.use("/articles", articleRouter);
-})
 
+})
+app.use("/articles", articleRouter);
 app.listen(3000);
